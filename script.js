9ac6d94d85542804d4d02c0e71b0d0bdee97a62e
Wyatt Salmons <wyattsalmons@gmail.com>
2019-02-04 09:29:17 -0700

    snap challenge 2/4

--- script.js
+++ script.js
@@ -7,11 +7,10 @@ const result = array.filter(newArray => newArray.userId != 5);
 console.log(result);
 */
 
-const str = ("Fate protects fools, little children and ships named Enterprise. You're going to be an interesting companion, Mr. Data. Some days you get the bear, and some days the bear gets you. Maybe if we felt any human loss as keenly as we feel one of those close to us, human history would be far less bloody. Why don't we just give everybody a promotion and call it a night - 'Commander'? Well, that's certainly good to know. Mr. Worf, you do remember how to fire phasers? A lot of things can change in twelve years, Admiral.");
+let str = ("Fate protects fools, little children and ships named Enterprise. You're going to be an interesting companion, Mr. Data. Some days you get the bear, and some days the bear gets you. Maybe if we felt any human loss as keenly as we feel one of those close to us, human history would be far less bloody. Why don't we just give everybody a promotion and call it a night - 'Commander'? Well, that's certainly good to know. Mr. Worf, you do remember how to fire phasers? A lot of things can change in twelve years, Admiral.");
 
-function filterArray() {
-	array = str.split(" ");
-	return `array`.toLowerCase() != 'f';
-}
+const array = str.toLowerCase().split(" ");
 
-console.log(filterArray());
+newArray = array.filter(index => !index.startsWith("f"));
+
+console.log(newArray);
